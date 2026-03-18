8b621d4a42da24d2de5ca1e34a9704dfe96f339a
kdw8309 <kdw8309@gmail.com>
2026-03-18 17:36:00 +0900

    김동욱 0318

---
+++ src/main/java/com/smp/controller/SMPController.java
@@ -0,0 +1,18 @@
+package com.smp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class SMPController {
+	
+	//메인페이지
+	@GetMapping("/")
+	public String main() {
+		return "main";
+	}
+	
+}
